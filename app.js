e29005cf89112b6b6734ec542850fd45076e4496
Ahan Mukhopadhyay <Ahan@Ahans-MacBook-Pro.local>
2016-08-30 13:43:43 +0530

    Updated Terminal Emulator

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ particlesJS.load('particles-js', './assets/particles.json', function() {
   console.log('callback - particles.js config loaded');
 });
 
+// For button scrolls
 var scrollToProjects = function() {
 	$('html,body').animate({
 			scrollTop: $('#projects').offset().top
@@ -20,3 +21,50 @@ var scrollToContact = function() {
 			scrollTop: $('#contact').offset().top
 		}, 1000);
 }
+
+// To Change CSS when Terminal is in focus
+$('.termbody').focusin(function() {
+	$(this).children('div').addClass('active');
+	$('.termheader').addClass('active');
+	$('.termheader').children('button').removeClass('grey');
+	// $('.terminput').focus();
+});
+
+$('.termbody').focusout(function() {
+	$(this).children('div').removeClass('active');
+	$('.termheader').removeClass('active');
+	$('.termheader').children('button').addClass('grey')
+});
+
+document.getElementById('termheader').onclick = function () {
+    document.getElementById('termbody').focus();
+};
+
+// For the blinking cursor and input focus on the Terminal
+$(function() {
+	$('#termbody').click(function() {
+		$('.terminput').focus();
+
+		cursor = window.setInterval(function() {
+			if ($('#cursor').css('visibility') === 'visible') {
+				$('#cursor').css({ visibility: 'hidden' });
+			} else {
+				$('#cursor').css({ visibility: 'visible' });  
+			}  
+		},500);
+
+	});
+
+	$('.terminput').keyup(function() {
+		console.log($(this).val());
+		$('#text').text($(this).val());
+
+	});
+
+	$('.terminput').blur(function() {
+		clearInterval(cursor);
+		$('#cursor').css({ visibility: 'visible' });    
+	});
+
+});
+
